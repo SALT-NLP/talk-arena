d356ad3dc0a9f8ccaa364080e5ff2508c204a681
yangxinsci1993 <ellaliminzhi@gmail.com>
2024-11-25 10:35:28 -0800

    update Typhoon based on their updated parameter names

--- src/talk_arena/streaming_helpers.py
+++ src/talk_arena/streaming_helpers.py
@@ -5,12 +5,25 @@ from pathlib import Path
 
 import google.generativeai as genai
 import gradio as gr
+import librosa
 import numpy as np
 import soundfile as sf
-
+import torch
 import xxhash
 from datasets import Audio
 from openai import OpenAI
+from transformers import (
+    AutoModel,
+    AutoModelForCausalLM,
+    AutoProcessor,
+    AutoTokenizer,
+    LlamaForCausalLM,
+    Qwen2AudioForConditionalGeneration,
+    TextIteratorStreamer,
+    WhisperForConditionalGeneration,
+)
+from transformers.generation import GenerationConfig
+
 
 def _get_config_for_model_name(model_id):
     if "API_MODEL_CONFIG" in os.environ:
@@ -82,8 +95,9 @@ def geminip_streaming(model_id):
         audio_bytes = Path(f"{x}.mp3").read_bytes()
         encoded_data = base64.b64encode(audio_bytes).decode("utf-8")
         prompt = "You are a helpful assistant. Respond conversationally to the speech provided."
+        text_response = []
         try:
-            response = client.chat.completions.create(
+            stream = client.chat.completions.create(
                 model="gemini-1.5-pro",
                 messages=[
                     {"role": "user", "content": prompt},
@@ -94,11 +108,14 @@ def geminip_streaming(model_id):
                         ],
                     },
                 ],
+                stream=True,
             )
-            # print('#Response', response.choices[0].message.content)
+            for chunk in stream:
+                if len(chunk.choices) > 0 and chunk.choices[0].delta.content is not None:
+                    text_response.append(chunk.choices[0].delta.content)
+                yield "".join(text_response)
+
             os.remove(f"{x}.mp3")
-            yield response.choices[0].message.content
-            return response.choices[0].message.content
         except:
             return "error"
 
@@ -173,9 +190,10 @@ def llm_streaming(model_id: str, prompt: str):
         return "error"
 
 
-def asr_streaming(model_id, asr_pipe):
+def asr_streaming(llm, asr_pipe):
     resampler = Audio(sampling_rate=16_000)
 
+    @torch.no_grad
     def pipelined(audio_input):
         if audio_input == None:
             return ""
@@ -208,7 +226,10 @@ def api_streaming(model_id):
         with open(f"{x}.wav", "rb") as wav_file:
             wav_data = wav_file.read()
         encoded_string = base64.b64encode(wav_data).decode("utf-8")
-        prompt = "You are a helpful assistant. Respond conversationally to the speech provided."
+        prompt = (
+            "You are a helpful assistant. Respond conversationally to the speech provided in the language it is"
+            " spoken in."
+        )
         try:
             completion = client.chat.completions.create(
                 model=model_id,
@@ -234,3 +255,161 @@ def api_streaming(model_id):
             return "error"
 
     return get_chat_response, client
+
+
+# Local Hosting Utilities
+
+
+def diva_streaming(diva_model_str):
+    diva_model = AutoModel.from_pretrained(diva_model_str, trust_remote_code=True)
+    resampler = Audio(sampling_rate=16_000)
+
+    @torch.no_grad
+    def diva_audio(audio_input, do_sample=False, temperature=0.001):
+        sr, y = audio_input
+        y = y.astype(np.float32)
+        y /= np.max(np.abs(y))
+        a = resampler.decode_example(resampler.encode_example({"array": y, "sampling_rate": sr}))
+        yield from diva_model.generate_stream(
+            a["array"],
+            (
+                "You are a helpful assistant The user is talking to you with their voice and you are responding with"
+                " text."
+            ),
+            do_sample=do_sample,
+            max_new_tokens=256,
+        )
+
+    return diva_audio, diva_model
+
+
+def typhoon_streaming(typhoon_model_str, device="cuda:1"):
+    typhoon_model = AutoModel.from_pretrained(typhoon_model_str, trust_remote_code=True).to(device)
+    tokenizer = typhoon_model.llama_tokenizer
+    resampler = Audio(sampling_rate=16_000)
+
+    @torch.no_grad
+    def typhoon_audio(audio_input, do_sample=False, temperature=0.001):
+        sr, y = audio_input
+        x = xxhash.xxh32(bytes(y)).hexdigest()
+        y = y.astype(np.float32)
+        y /= np.max(np.abs(y))
+        a = resampler.decode_example(resampler.encode_example({"array": y, "sampling_rate": sr}))
+        sf.write(f"{x}.wav", a["array"], a["sampling_rate"], format="wav")
+        streamer = TextIteratorStreamer(tokenizer)
+        prompt_pattern = (
+            "<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\n<Speech><SpeechHere></Speech>"
+            " {}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+        )
+        response = typhoon_model.generate(
+            wav_path=f"{x}.wav",
+            prompt=(
+                "You are a helpful assistant. Listen to this audio, and respond accordingly in the language it is"
+                " spoken in."
+            ),
+            device=device,
+            prompt_pattern=prompt_pattern,
+            do_sample=False,
+            max_length=1200,
+            num_beams=1,
+            streamer=streamer,  # supports TextIteratorStreamer
+        )
+        generated_text = ""
+        for new_text in streamer:
+            generated_text += new_text
+            yield generated_text.split("<|start_header_id|>assistant<|end_header_id|>\n\n")[-1].replace(
+                "<|eot_id|>", ""
+            )
+        os.remove(f"{x}.wav")
+        return generated_text.split("<|start_header_id|>assistant<|end_header_id|>\n\n")[-1].replace("<|eot_id|>", "")
+
+    return typhoon_audio, typhoon_model
+
+
+def qwen2_streaming(qwen2_model_str):
+    resampler = Audio(sampling_rate=16_000)
+    qwen2_processor = AutoProcessor.from_pretrained(qwen2_model_str)
+    qwen2_model = Qwen2AudioForConditionalGeneration.from_pretrained(qwen2_model_str, device_map="auto")
+    qwen2_model.generation_config = GenerationConfig.from_pretrained(
+        qwen2_model_str,
+        trust_remote_code=True,
+        do_sample=False,
+        top_k=50,
+        top_p=1.0,
+    )
+
+    @torch.no_grad
+    def qwen2_audio(audio_input, do_sample=False, temperature=0.001):
+        if audio_input == None:
+            return ""
+        sr, y = audio_input
+        x = xxhash.xxh32(bytes(y)).hexdigest()
+        y = y.astype(np.float32)
+        y /= np.max(np.abs(y))
+        a = resampler.decode_example(resampler.encode_example({"array": y, "sampling_rate": sr}))
+        sf.write(f"{x}.wav", a["array"], a["sampling_rate"], format="wav")
+        conversation = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "audio",
+                        "audio_url": f"{x}.wav",
+                    },
+                ],
+            },
+        ]
+        text = qwen2_processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
+        audios = [librosa.load(f"{x}.wav", sr=qwen2_processor.feature_extractor.sampling_rate)[0]]
+        inputs = qwen2_processor(text=text, audios=audios, return_tensors="pt", padding=True)
+        streamer = TextIteratorStreamer(qwen2_processor)
+        qwen2_model.generate(**inputs, streamer=streamer, max_length=256)
+        generated_text = ""
+        for new_text in streamer:
+            generated_text += new_text
+            yield generated_text.split("<|im_start|>assistant\n")[-1].replace("<|im_end|>", "")
+        os.remove(f"{x}.wav")
+        return generated_text
+
+    return qwen2_audio, qwen2_model
+
+
+def typhoon_streaming(typhoon_model_str):
+    resampler = Audio(sampling_rate=16_000)
+    typhoon_model = AutoModel.from_pretrained(typhoon_model_str, torch_dtype=torch.float16, trust_remote_code=True)
+    typhoon_model.to("cuda:0")
+    typhoon_model.eval()
+    prompt_pattern = (
+        "<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\n<Speech><SpeechHere></Speech>"
+        " {}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+    )
+    prompt = (
+        "You are a helpful assistant. Respond conversationally to the speech provided in the language it is spoken in."
+    )
+
+    @torch.no_grad
+    def typhoon_audio(audio_input, do_sample=False, temperature=0.001):
+        if audio_input == None:
+            return ""
+        sr, y = audio_input
+        x = xxhash.xxh32(bytes(y)).hexdigest()
+        y = y.astype(np.float32)
+        y /= np.max(np.abs(y))
+        a = resampler.decode_example(resampler.encode_example({"array": y, "sampling_rate": sr}))
+        sf.write(f"{x}.wav", a["array"], a["sampling_rate"], format="wav")
+        print(f"{x}.wav")
+        response = typhoon_model.generate(
+            audio=a["array"],
+            prompt=prompt,
+            prompt_pattern=prompt_pattern,
+            do_sample=False,
+            max_new_tokens=512,
+            repetition_penalty=1.1,
+            num_beams=1,
+        )
+        yield response
+        os.remove(f"{x}.wav")
+        return response
+
+    return typhoon_audio, typhoon_model
